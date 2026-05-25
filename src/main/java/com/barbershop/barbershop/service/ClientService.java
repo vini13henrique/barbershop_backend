dddf6562cb5649fe1c6a70bfc1c1f469496e6969
vinicius henrique <vinicius13henrique01@gmail.com>
2026-05-24 22:25:30 -0300

    feat: adicionada validação manual no create client

--- src/main/java/com/barbershop/barbershop/service/ClientService.java
+++ src/main/java/com/barbershop/barbershop/service/ClientService.java
@@ -22,10 +22,22 @@ public class ClientService {
     }
 
     public Client createClient(ClientRequestDTO client) {
-
         Client clientEntity = new Client();
+        String nome = client.getName();
+        String phone = client.getPhone();
+
+        //validaçap nome não pode ser nulo ou está vazio
+        if (nome == null || nome.trim().isEmpty()) {
+            throw new RuntimeException("Erro: nome não pode ser nulo ou  está vazio");
+        }
+
+        //validaçao phone não pode ser nulo ou está vazio
+        if (phone == null || phone.trim().isEmpty()) {
+            throw new RuntimeException("Erro: Número de telefone não poder ser nulo ou está vazio");
+        }
         clientEntity.setName(client.getName());
         clientEntity.setPhone(client.getPhone());
         return clientRepository.save(clientEntity);
+
     }
 }
